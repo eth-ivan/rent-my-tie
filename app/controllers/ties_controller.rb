e5035424a416fcae4384cb85bec96f3bd1975e21
Ivan Perroud <ivan@perroud.me>
2021-02-25 15:18:23 +0100

    added simple form with desc and price_per_day

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -2,4 +2,22 @@ class TiesController < ApplicationController
   def index
   end
 
+  def new
+    @tie = Tie.new
+  end
+
+  def create
+    @tie = Tie.new(tie_params)
+    if @tie.save
+      redirect_to @tie
+    else
+      render :new # il faut render pour afficher le message d'erreur
+    end
+  end
+
+  private
+
+  def tie_params
+    params.require(:tie).permit(:description, :price_per_day)
+  end
 end
