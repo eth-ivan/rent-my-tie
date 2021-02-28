b7438465f203c3831d15cd121ce70636e2f6878b
Antoine Robert <antoinero@gmail.com>
2021-02-28 13:36:06 +0100

    show done

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ties
   # verb 'path', to: 'controller#action'
+
+  get "ties/:id", to: "ties#show", as: :tie
 end
