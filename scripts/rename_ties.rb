cfa7a9e7559b5eaecd4fc817e1b2903bbddd9159
Ivan Perroud <ivan@perroud.me>
2021-02-28 11:21:22 +0100

    with names and color

--- scripts/rename_ties.rb
+++ scripts/rename_ties.rb
@@ -4,9 +4,9 @@ folder_path = "/Users/ivanperroud/code/eth-ivan/rent-my-tie/app/assets/images/on
 puts "Renaming files..."
 
 Dir.glob(folder_path + "/*").each do |file|
-  new_filename = Faker::Beer.hop
-  File.rename(file, folder_path + "/" + new_filename + File.extname(file))
-  puts new_filename
+  name = Faker::Artist.name + " " + Faker::Color.color_name
+  new_file = folder_path + "/" + name + File.extname(file)
+  File.rename(file, new_file)
 end
 
 puts "Renaming complete."
