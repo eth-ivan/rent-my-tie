a77c64ea3ce27f5799331bc783f09d1cc703f875
Ivan Perroud <ivan@perroud.me>
2021-02-28 11:31:27 +0100

    corrections in script

--- scripts/rename_ties.rb
+++ scripts/rename_ties.rb
@@ -1,11 +1,12 @@
 require "faker"
-folder_path = "/Users/ivanperroud/code/eth-ivan/rent-my-tie/app/assets/images/one_hundred_ties"
+
+images_path = File.expand_path("..", Dir.pwd) + "/app/assets/images/one_hundred_ties"
 
 puts "Renaming files..."
 
-Dir.glob(folder_path + "/*").each do |file|
+Dir.glob(images_path + "/*").each do |file|
   name = Faker::Artist.name + " " + Faker::Color.color_name
-  new_file = folder_path + "/" + name + File.extname(file)
+  new_file = images_path + "/" + name + File.extname(file)
   File.rename(file, new_file)
 end
 
