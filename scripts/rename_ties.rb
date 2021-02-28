851471068c7988cfbee0762a4b0da3b28397de99
Ivan Perroud <ivan@perroud.me>
2021-02-28 11:13:24 +0100

    added 100 ties with faker

--- scripts/rename_ties.rb
+++ scripts/rename_ties.rb
@@ -1,10 +1,11 @@
+require "faker"
+folder_path = "/Users/ivanperroud/code/eth-ivan/rent-my-tie/app/assets/images/one_hundred_ties"
+
 puts "Renaming files..."
 
-folder_path = "/Users/ivanperroud/code/eth-ivan/rent-my-tie/app/assets/images/one_hundred_ties"
-Dir.glob(folder_path + "/*").sort.each_with_index do |f, index|
-  filename = File.basename(f, File.extname(f))
-  new_filename = "tie" + index.to_s
-  File.rename(f, folder_path + "/" + new_filename + File.extname(f))
+Dir.glob(folder_path + "/*").each do |file|
+  new_filename = Faker::Beer.hop
+  File.rename(file, folder_path + "/" + new_filename + File.extname(file))
   puts new_filename
 end
 
