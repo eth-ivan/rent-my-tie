2e85134d0fe8ed1555babc9572857fdcf8f2d7de
ismael gashi <i.gashi@pm.me>
2021-03-04 21:02:55 +0100

    Merged master again

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  resources :ties
   resources :bookings
-=======
 
   resources :ties, only: [:index, :show, :new, :create]
 
->>>>>>> master
   # verb 'path', to: 'controller#action'
 
   # get "ties/:id", to: "ties#show", as: :tie
