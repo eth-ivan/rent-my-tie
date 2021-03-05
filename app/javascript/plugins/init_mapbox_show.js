d5440220d73f315311675161fa0c5f783f4d887d
Ivan Perroud <ivan@perroud.me>
2021-03-05 22:52:47 +0100

    adjust zoom show

--- app/javascript/plugins/init_mapbox_show.js
+++ app/javascript/plugins/init_mapbox_show.js
@@ -20,7 +20,7 @@ const addMarkersToMap = (map, marker) => {
 const fitMapToMarkers = (map, marker) => {
   const bounds = new mapboxgl.LngLatBounds();
   bounds.extend([marker.lng, marker.lat])
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
 };
 
 const initMapboxShow = () => {
